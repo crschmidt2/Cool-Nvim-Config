a82fac725a3dedba814b01216f3671dd944b9463
crschmidt2 <115666285+crschmidt2@users.noreply.github.com>
2023-05-27 22:50:58 -0500

    Initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  -- { "ellisonleao/gruvbox.nvim", lazy = false, priority = 1000, opts = {} },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "slate",
+    },
+  },
+}
