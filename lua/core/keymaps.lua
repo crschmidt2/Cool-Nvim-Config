0bb479eedf1e78e9bcfaa9071fff2407827e3470
crschmidt2 <115666285+crschmidt2@users.noreply.github.com>
2024-01-14 15:51:05 -0600

    Added Ctrl+S = save

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,6 +7,8 @@ vim.keymap.set('','<leader>p', '"0p')
 --Clear search highlights
 vim.keymap.set('', '<leader>h', ':noh<CR>')
 
+vim.keymap.set('', '<C-s>', ':w<CR>')
+
 --Funny
 vim.keymap.set('n', '<leader>lhs', function() print("Tyler is a poopy head") end)
 
