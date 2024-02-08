23ea569f9696f60d1633fb6671605bab5f382ddc
crschmidt2 <115666285+crschmidt2@users.noreply.github.com>
2024-02-07 23:44:06 -0600

    Fixed filetree keybindings

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -1,14 +1,35 @@
+local my_on_attach = function(bufnr)
+				local api = require "nvim-tree.api"
+
+				--local function opts(desc)
+      --return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    --end
+
+    api.config.mappings.default_on_attach(bufnr)
+
+				vim.keymap.del("n", "<C-e>", {buffer = bufnr})
+end
+
 return {
 				{
 "nvim-tree/nvim-tree.lua",
 keys = {
-				{"<C-e>", "<cmd>NvimTreeToggle<CR>"},
+				{"<C-e>", "<cmd>NvimTreeToggle<CR>", desc = "Open file tree"},
 },
 opts = {
 hijack_cursor = true,
-view = {
-				cursorline = true
+renderer = {
+add_trailing = true,
+icons = {
+show = {
+folder_arrow = false,
 },
+},
+indent_markers = {
+enable = true,
+},
+},
+on_attach = my_on_attach,
 },
 config = function(_, opts)
 				require("nvim-tree").setup(opts)
