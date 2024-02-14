e909629109e14c2627b84958f1705a85221e9acd
crschmidt2 <115666285+crschmidt2@users.noreply.github.com>
2024-02-14 11:00:44 -0600

    CMP configuration
    Added typescript configuration. Edited cmp keybinds.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,70 +1,71 @@
+
 --TODO: Edit keybinds to use telescope versions of commands when applicable/preferable
 --TODO: Edit cmp keybinds
+return {
+	{
+		"williamboman/mason.nvim",
+		opts = {},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			--This is my folder for configuration of lsps instead of doing them all in this file
+			--require("core.lspconfig")
 
-return { {
-	"neovim/nvim-lspconfig",
-				dependencies = {"williamboman/mason.nvim"},
+			--CUSTOM FUNCTION FOR HOVER
+			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+				vim.lsp.handlers.hover, {
+					-- Use a sharp border with `FloatBorder` highlights
+					border = "single",
+					-- add the title in hover float window
+					--title = "Title"
+				}
+			)
 
-	config = function()
-		--This is my folder for configuration of lsps instead of doing them all in this file
-		require("core.lspconfig")
+			-- Global mappings.
+			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+			--vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+			--vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+			--vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+			--vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
-		--CUSTOM FUNCTION FOR HOVER
-		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-			vim.lsp.handlers.hover, {
-				-- Use a sharp border with `FloatBorder` highlights
-				border = "single",
-				-- add the title in hover float window
-				--title = "Title"
-			}
-		)
+			-- Use LspAttach autocommand to only map the following keys
+			-- after the language server attaches to the current buffer
+			vim.api.nvim_create_autocmd('LspAttach', {
+				group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+				callback = function(ev)
+					-- Enable completion triggered by <c-x><c-o>
+					vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-		-- Global mappings.
-		-- See `:help vim.diagnostic.*` for documentation on any of the below functions
-		--vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-		--vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-		--vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-
-
-		--vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
-		-- Use LspAttach autocommand to only map the following keys
-		-- after the language server attaches to the current buffer
-		vim.api.nvim_create_autocmd('LspAttach', {
-			group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-			callback = function(ev)
-				-- Enable completion triggered by <c-x><c-o>
-				vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-				-- Buffer local mappings.
-				-- See `:help vim.lsp.*` for documentation on any of the below functions
-				local opts = { buffer = ev.buf }
-				vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-				vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-				vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-				vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-				--vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-				vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-				vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-				vim.keymap.set('n', '<space>wl', function()
-					print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-				end, opts)
-				vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-				vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-				vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-				vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-				vim.keymap.set('n', '<space>f', function()
-					vim.lsp.buf.format { async = true }
-				end, opts)
-			end,
-		})
-	end
-
-},
+					-- Buffer local mappings.
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+					local opts = { buffer = ev.buf }
+					vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+					vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+					vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+					--vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+					vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+					vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+					vim.keymap.set('n', '<space>wl', function()
+						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+					end, opts)
+					vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+					vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+					vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+					vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+					vim.keymap.set('n', '<space>f', function()
+						vim.lsp.buf.format { async = true }
+					end, opts)
+				end,
+			})
+		end
+	},
 	{
 		"hrsh7th/nvim-cmp",
 		event = "InsertEnter",
-		dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "hrsh7th/cmp-cmdline", },
+		dependencies = { "neovim/nvim-lspconfig", "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "hrsh7th/cmp-cmdline", },
 		config = function()
 			local cmp = require 'cmp'
 
@@ -79,15 +80,14 @@ return { {
 					end,
 				},
 				window = {
-					-- completion = cmp.config.window.bordered(),
+					completion = cmp.config.window.bordered(),
 					-- documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
-					['<C-b>'] = cmp.mapping.scroll_docs(-4),
-					['<C-f>'] = cmp.mapping.scroll_docs(4),
+					['<Tab>'] = cmp.mapping.select_next_item(),
+					['<S-Tab>'] = cmp.mapping.select_prev_item(),
 					['<C-Space>'] = cmp.mapping.complete(),
-					['<C-e>'] = cmp.mapping.abort(),
-					['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 				}),
 				sources = cmp.config.sources({
 					{ name = 'nvim_lsp' },
@@ -127,12 +127,15 @@ return { {
 				})
 			})
 
-			-- Set up lspconfig.
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
-			-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-			require('lspconfig')['lua-language-server'].setup {
-				capabilities = capabilities
-			}
+			--INSERT PARENTHESIS AFTER FUNCTION OR METHOD
+			local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+			cmp.event:on(
+				'confirm_done',
+				cmp_autopairs.on_confirm_done()
+			)
+
+			--DIRECTORY FOR INDIVIDUAL LSP CONFIGURATIONS
+			require("core.lspconfig")
 		end
 	},
 }
