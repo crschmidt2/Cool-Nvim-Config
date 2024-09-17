204bb0b16a8b28c18854f438c18d642654e99c73
crschmidt2 <115666285+crschmidt2@users.noreply.github.com>
2024-09-17 14:43:24 -0500

    Fixed typescript server and added themery

--- lua/core/lspconfig/typescriptserver.lua
+++ lua/core/lspconfig/typescriptserver.lua
@@ -8,7 +8,7 @@
 local vue_language_server_path = vim.fn.stdpath("data") ..
     "/mason/packages/vue-language-server/node_modules/@vue/language-server"
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-require 'lspconfig'.tsserver.setup {
+require 'lspconfig'.ts_ls.setup {
   capabilities = capabilities,
   init_options = {
     plugins = {
