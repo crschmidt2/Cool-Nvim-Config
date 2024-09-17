5429f38f19f148ab233d45ab67e9ffbfcb2dc11e
crschmidt2 <115666285+crschmidt2@users.noreply.github.com>
2024-09-17 16:46:11 -0500

    In progress gruvbox and theme stuff

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,14 +8,20 @@ return
 {
   {
     "zaldih/themery.nvim",
+    priority = 1000,
     opts = {
-      themes = { "tokyonight", "onedark", "ayu" },
+      themes = { "tokyonight", "onedark", "ayu", "gruvbox" },
       livePreview = true
-    }
+    },
+    config = function(_, opts)
+      vim.keymap.set('n', '<leader>cs', '<cmd>Themery<CR>')
+      require("themery").setup(opts)
+    end
   },
   {
     "folke/tokyonight.nvim",
     lazy = true,
+    priority = 999,
     opts = {
       style = "storm",
       on_colors = function(c)
@@ -33,13 +39,13 @@ return
   {
     "navarasu/onedark.nvim",
     lazy = true,
+    priority = 999,
     opts = {
       style = "cool",
       colors = {
-        borderColor = "#ffffff",
+        borderColor = "#FFFFFF",
         nontext = "#242b38"
       },
-
       highlights = {
         ["FloatBorder"] = { fg = '$borderColor', bg = '$nontext' },
         ["TelescopePreviewBorder"] = { fg = '$borderColor', bg = '$nontext' },
@@ -48,16 +54,26 @@ return
         ["NvimTreeIndentMarker"] = { fg = 'Gray31' },
       },
     },
+    config = true,
   },
   {
     "Shatur/neovim-ayu",
     lazy = true,
-    -- priority = 1000,
+    priority = 999,
     opts = {
       mirage = true,
     },
     config = function(_, opts)
       require("ayu").setup(opts)
+    end
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 999,
+    config = function()
+      require("gruvbox").setup()
+      vim.api.nvim_set_hl(0, "FloatBorder", { fg = "Red" })
+      vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = "Red" })
     end
   }
 }
