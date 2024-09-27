47de3f843fc78f8ce266adbab0dfe4c0bf3c1205
crschmidt2 <115666285+crschmidt2@users.noreply.github.com>
2024-09-27 17:43:47 -0500

    Update csv.lua

--- lua/plugins/csv.lua
+++ lua/plugins/csv.lua
@@ -1,19 +1,25 @@
 return {
-    'cameron-wags/rainbow_csv.nvim',
-    config = true,
-    ft = {
-        'csv',
-        'tsv',
-        'csv_semicolon',
-        'csv_whitespace',
-        'csv_pipe',
-        'rfc_csv',
-        'rfc_semicolon'
-    },
-    cmd = {
-        'RainbowDelim',
-        'RainbowDelimSimple',
-        'RainbowDelimQuoted',
-        'RainbowMultiDelim'
-    }
+  'cameron-wags/rainbow_csv.nvim',
+  ft = {
+    'csv',
+    'tsv',
+    'csv_semicolon',
+    'csv_whitespace',
+    'csv_pipe',
+    'rfc_csv',
+    'rfc_semicolon'
+  },
+  keys = {
+    { '<leader>cp', '<cmd>set ft=csv_pipe<CR>', desc = 'Set filetype to csv_pipe' },
+  },
+  cmd = {
+    'RainbowDelim',
+    'RainbowDelimSimple',
+    'RainbowDelimQuoted',
+    'RainbowMultiDelim'
+  },
+  config = function()
+    vim.g.rbql_backend_language = 'js'
+    require("rainbow_csv").setup()
+  end
 }
