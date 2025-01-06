b11c4652b0844ced483b3774e0966b15f8bb7553
Corey Schmidt <coreyr.schmidt@gmail.com>
2025-01-06 00:16:20 -0600

    Heirline filename edit

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,7 +3,17 @@ vim.opt.clipboard = "unnamedplus"
 
 --Remove comment continuation on newline
 --DEFAULT: jcroql (see :help fo-table)
-vim.opt.formatoptions = "jql"
+
+--This stops newlines from continuing comments. Not sure why the vim.opt.formatoptions is not working
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = vim.api.nvim_create_augroup("FormatOptions", { clear = true }),
+  pattern = { "*" },
+  callback = function()
+    vim.opt_local.fo:remove("c")
+    vim.opt_local.fo:remove("o")
+    vim.opt_local.fo:remove("r")
+  end,
+})
 
 --LINE NUMBER
 vim.opt.number = true
@@ -30,3 +40,7 @@ vim.opt.inccommand = ""
 
 --SCROLLING
 vim.opt.scrolloff = 4
+
+--CMD LINE
+vim.opt.cmdheight = 0
+vim.opt.shortmess:append('W')
--- lua/plugins/heirline/statusline.lua
+++ lua/plugins/heirline/statusline.lua
@@ -2,13 +2,7 @@ local conditions = require("heirline.conditions")
 
 local ViMode = require("plugins.heirline.components.vi-mode")
 
-local FileName = {
-  provider = "%f",
-  -- hl = {
-  --   fg = "purple",
-  --   bg = "default_bg",
-  -- }
-}
+local FileName = require("plugins.heirline.components.filename")
 
 local Align = { provider = "%=" }
 local Space = { provider = " " }
@@ -27,9 +21,7 @@ local InactiveStatusLine = {
   condition = conditions.is_not_active,
 }
 
-
 return {
-
   hl = function()
     local foreground = ""
     if conditions.is_active() then
