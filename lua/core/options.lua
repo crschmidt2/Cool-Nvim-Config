17d2ccb91ac3226c9de2c65b07413ae6e51776c2
crschmidt2 <115666285+crschmidt2@users.noreply.github.com>
2025-01-29 08:41:13 -0600

    Updated tab width

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -29,8 +29,8 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 --TABS & INDENT
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.wrap = false
 
