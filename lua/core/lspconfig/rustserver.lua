1e88bfd72bebfb44ac99fc9f52c0d689c48746af
Corey Schmidt <cschmidt@arcbtech.com>
2025-08-13 17:03:52 -0500

    Update lazy-lock.json and rust lsp

--- lua/core/lspconfig/rustserver.lua
+++ lua/core/lspconfig/rustserver.lua
@@ -1,5 +1,9 @@
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+local rust_ls_path = vim.fn.stdpath("data") ..
+    "/mason/bin/rust-analyzer"
+
 require 'lspconfig'.rust_analyzer.setup {
     capabilities = capabilities,
+    cmd = { rust_ls_path }
 }
