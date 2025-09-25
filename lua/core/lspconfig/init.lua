8345e5a740e4c141933e26d8ada1dcc267a23255
Corey Schmidt <coreyr.schmidt@gmail.com>
2025-09-24 21:46:16 -0500

    edited lspconfig/init.lua

--- lua/core/lspconfig/init.lua
+++ lua/core/lspconfig/init.lua
@@ -1,17 +1,1 @@
-require("core.lspconfig.luaserver")
-require("core.lspconfig.htmlserver")
 require("core.lspconfig.typescriptserver")
-
--- C# & DOTNET
--- MAKE SURE YOU INSTALL ROSLYN AND RZLS (configured in roslyn.lua package)
--- require("core.lspconfig.csharpserver")
--- require("core.lspconfig.omnisharp")
-
-require("core.lspconfig.cpp")
-require("core.lspconfig.arduinoserver")
-
--- Enable if working with Vue 2
--- require("core.lspconfig.vue2server")
-
-require("core.lspconfig.rustserver")
-require("core.lspconfig.latex")
