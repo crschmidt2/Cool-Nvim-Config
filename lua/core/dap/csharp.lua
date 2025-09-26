38ec8b79f9460ab65cf55fc25ea4f772f93b0c95
Corey Schmidt <cschmidt@arcbtech.com>
2025-09-26 18:44:12 -0500

    Edited dap configuration

--- lua/core/dap/csharp.lua
+++ lua/core/dap/csharp.lua
@@ -2,6 +2,12 @@ local dap = require('dap')
 local net_core_bd_path = vim.fn.stdpath("data") ..
     "/mason/bin/netcoredbg"
 
+--Hacky way to get nvim-dap working with Mason
+local osName = vim.loop.os_uname().sysname
+if osName == 'Windows_NT' then
+    net_core_bd_path = net_core_bd_path .. '.cmd'
+end
+
 dap.adapters.coreclr = {
     type = 'executable',
     command = net_core_bd_path,
@@ -9,12 +15,12 @@ dap.adapters.coreclr = {
 }
 
 dap.configurations.cs = {
-  {
-    type = "coreclr",
-    name = "launch - netcoredbg",
-    request = "launch",
-    program = function()
-        return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
-    end,
-  },
+    {
+        type = "coreclr",
+        name = "launch - netcoredbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+        end,
+    },
 }
--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,6 +1,33 @@
+local dap = require('dap')
+
 return {
     {
         'mfussenegger/nvim-dap',
+        keys = {
+            { '<F5>',  dap.continue,  desc = 'Start/continue debugging' },
+            { '<F10>', dap.step_over, desc = 'Step over' },
+            { '<F11>', dap.step_into, desc = 'Step into' },
+            { '<F12>', dap.step_out,  desc = 'Step out' },
+            { '<leader>b', dap.toggle_breakpoint,  desc = 'Toggle breakpoint' },
+            { '<leader>dk', dap.run_to_cursor,  desc = 'Run to cursor' },
+        },
+        cmd = {
+            'DapNew',
+            'DapPause',
+            'DapShowLog',
+            'DapStepOut',
+            'DapContinue',
+            'DapStepInto',
+            'DapStepOver',
+            'DapTerminate',
+            'DapDisconnect',
+            'DapToggleRepl',
+            'DapSetLogLevel',
+            'DapRestartFrame',
+            'DapClearBreakpoints',
+            'DapToggleBreakpoint',
+            'DapEval'
+        },
         config = function(_, opts)
             require('core.dap')
         end
