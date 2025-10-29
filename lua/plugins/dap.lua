7c05d3f5426029476b39ce6881763d0aba9a1cba
Corey Schmidt <cschmidt@arcbtech.com>
2025-10-29 10:40:09 -0500

    Fixed autopairs and changed dap terminal keybind

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -21,10 +21,9 @@ return {
             vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Step into" })
             vim.keymap.set("n", "<C-,>", dap.step_out, { desc = "Step out" })
             vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Step out" })
-            -- vim.keymap.del("n", "<CR>")
-            vim.keymap.set("n", "q", function()
+            vim.keymap.set("n", "<leader>dt", function()
                 dap.terminate()
-            end, { desc = "Terminate and clear breakpoints" })
+            end, { desc = "Terminate" })
         end
     },
     {
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -77,10 +77,10 @@ return {
             -- vim.keymap.set("n", "<leader>cp", "<cmd>AutolistCycleNext<cr>")
 
             -- functions to recalculate list on edit
-            vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
-            vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
-            vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
-            vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
+            -- vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
+            -- vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
+            -- vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
+            -- vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
 
             vim.api.nvim_create_autocmd('InsertLeave', {
                 pattern = { '*.md' },
