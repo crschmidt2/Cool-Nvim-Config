b8755c1ef0e6e501359eede614f95bdf9ae6ee6c
Corey Schmidt <coreyr.schmidt@gmail.com>
2026-06-20 23:28:02 -0500

    Fixed auto session and treesitter

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,13 +1,12 @@
 return {
     "rmagatti/auto-session",
     lazy = false,
+    ---enables autocomplete for opts
     ---@module "auto-session"
     ---@type AutoSession.Config
     opts = {
-        enabled = true,
+        suppressed_dirs = { "~/", "~/projects", "~/Downloads", "/" },
         auto_restore_last_session = false,
+        -- log_level = 'debug',
     },
-    config = function(_, opts)
-        require('auto-session').setup(opts)
-    end
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,71 +1,82 @@
-return { {
-    "nvim-treesitter/nvim-treesitter",
-    enabled = true,
-    event = { "BufReadPost", "BufReadPre", "BufNewFile", "BufWritePre" },
-    -- lazy = vim.fn.argc(-1) == 0,
-    build = ":TSUpdate",
-    branch = "master",
-    version = false,
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    keys = {
-        { "<c-space>", desc = "Increment Selection" },
-        { "<bs>",      desc = "Decrement Selection", mode = "x" },
-    },
-    opts = {
-        ensure_installed = { "lua", "luadoc", "markdown", "json", "vimdoc", "javascript", "typescript", "rust", "c_sharp", "vue", "html", "css", "cpp", "printf", "vim", "vimdoc" },
-        highlight = { enable = true, use_languagetree = true },
-        indent = { enable = false },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<A-o>",
-                node_incremental = "<A-o>",
-                scope_incremental = "<A-p>",
-                node_decremental = "<A-i>",
-            },
-        },
-    },
+return {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate',
+    opts = {},
     config = function(_, opts)
-        require 'nvim-treesitter.install'.compilers = { "clang", "gcc" }
-        require 'nvim-treesitter.configs'.setup(opts)
-        vim.filetype.add({
-            pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
-        })
-
-        -- CLASSIC ASP HIGHLIGHTING
-        vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
-            callback = function()
-                if vim.opt.filetype:get() == 'aspvbs' then
-                    vim.api.nvim_command('set ft=vb')
-                end
-            end
-        })
+        require('nvim-treesitter').setup()
+        require('nvim-treesitter').install { 'vim', 'vimdoc', 'javascript', 'typescript', 'rust', 'c_sharp', 'nix', 'markdown', 'json', 'css', 'html', 'luadoc', 'vue' }
     end
-},
-    {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter"
-        },
-        event = { "BufReadPost", "BufReadPre", "BufNewFile", "BufWritePre" },
-        config = function()
-            require 'nvim-treesitter.configs'.setup {
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["ac"] = "@class.outer",
-                            ["ic"] = "@class.inner",
-                            ["ip"] = "@parameter.inner",
-                            ["ap"] = "@parameter.outer",
-                        },
-                        include_surrounding_whitespace = true,
-                    }
-                }
-            }
-        end
-    }
 }
+--OLD TREESITTER CONFIG
+-- return { {
+--     "nvim-treesitter/nvim-treesitter",
+--     enabled = true,
+--     event = { "BufReadPost", "BufReadPre", "BufNewFile", "BufWritePre" },
+--     -- lazy = vim.fn.argc(-1) == 0,
+--     build = ":TSUpdate",
+--     branch = "master",
+--     version = false,
+--     cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+--     keys = {
+--         { "<c-space>", desc = "Increment Selection" },
+--         { "<bs>",      desc = "Decrement Selection", mode = "x" },
+--     },
+--     opts = {
+--         ensure_installed = { "lua", "luadoc", "markdown", "json", "vimdoc", "javascript", "typescript", "rust", "c_sharp", "vue", "html", "css", "cpp", "printf", "vim", "vimdoc" },
+--         highlight = { enable = true, use_languagetree = true },
+--         indent = { enable = false },
+--         incremental_selection = {
+--             enable = true,
+--             keymaps = {
+--                 init_selection = "<A-o>",
+--                 node_incremental = "<A-o>",
+--                 scope_incremental = "<A-p>",
+--                 node_decremental = "<A-i>",
+--             },
+--         },
+--     },
+--     config = function(_, opts)
+--         require 'nvim-treesitter.install'.compilers = { "clang", "gcc" }
+--         -- require 'nvim-treesitter.configs'.setup(opts)
+--         vim.filetype.add({
+--             pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+--         })
+--
+--         -- CLASSIC ASP HIGHLIGHTING
+--         vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
+--             callback = function()
+--                 if vim.opt.filetype:get() == 'aspvbs' then
+--                     vim.api.nvim_command('set ft=vb')
+--                 end
+--             end
+--         })
+--     end
+-- },
+--     {
+--         "nvim-treesitter/nvim-treesitter-textobjects",
+--         dependencies = {
+--             "nvim-treesitter/nvim-treesitter"
+--         },
+--         event = { "BufReadPost", "BufReadPre", "BufNewFile", "BufWritePre" },
+--         config = function()
+--             -- require 'nvim-treesitter.configs'.setup {
+--             --     textobjects = {
+--             --         select = {
+--             --             enable = true,
+--             --             lookahead = true,
+--             --             keymaps = {
+--             --                 ["af"] = "@function.outer",
+--             --                 ["if"] = "@function.inner",
+--             --                 ["ac"] = "@class.outer",
+--             --                 ["ic"] = "@class.inner",
+--             --                 ["ip"] = "@parameter.inner",
+--             --                 ["ap"] = "@parameter.outer",
+--             --             },
+--             --             include_surrounding_whitespace = true,
+--             --         }
+--             --     }
+--             -- }
+--         end
+--     }
+-- }
